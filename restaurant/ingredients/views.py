35f2e9347748c9edb7d5644edadd7e56d302072f
Alejandro Duque <ducode@outlook.com>
2021-08-06 17:21:27 -0500

    general models refactor.

--- restaurant/ingredients/views.py
+++ restaurant/ingredients/views.py
@@ -5,22 +5,22 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse, reverse_lazy
-from recipes.models import Recipe_Ingredient
+# from recipes.models import Recipe_Ingredient
 
-# Create your views here.
+# # Create your views here.
 
 class IngredientView(LoginRequiredMixin, ListView):
     """Return all published posts."""
 
     template_name = 'ingredients/ingredients.html'
-    model = Recipe_Ingredient
-    queryset = Ingredient.objects.all()
-    ordering = ('-created',)
-    context_object_name = 'ingredients'
+#     model = Recipe_Ingredient
+#     queryset = Ingredient.objects.all()
+#     ordering = ('-created',)
+#     context_object_name = 'ingredients'
 
 
-class IngredientCreateView(LoginRequiredMixin, CreateView):
-    model = Ingredient
-    fields = ['ingredient_name','unit_of_measure']
-    template_name = 'ingredients/ingredientcreate.html'
-    success_url = reverse_lazy('ingredients:ingredientslist')
+# class IngredientCreateView(LoginRequiredMixin, CreateView):
+#     model = Ingredient
+#     fields = ['ingredient_name','unit_of_measure']
+#     template_name = 'ingredients/ingredientcreate.html'
+#     success_url = reverse_lazy('ingredients:ingredientslist')
