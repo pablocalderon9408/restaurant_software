fc9a2c8d9771312d556fddd005d8ce0fea879a8e
pablocalderon9408 <pablocalderon9408@gmail.com>
2021-08-24 00:14:48 -0500

    Start with Forms (Focus on ingredients app) to input data to the different models.

--- restaurant/ingredients/views.py
+++ restaurant/ingredients/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from pdb import set_trace
+from typing import ContextManager
 from ingredients.models import Ingredient, Stock
 from recipes.models import RecipeIngredient
 from django.shortcuts import render
@@ -6,9 +8,24 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse, reverse_lazy
+from ingredients.forms import IngredientUnitForm, IngredientUnitForm, StockForm
 # from recipes.models import Recipe_Ingredient
 
 # # Create your views here.
+
+class IngredientUnitCreateView(LoginRequiredMixin, CreateView):
+    form_class = IngredientUnitForm
+    template_name = 'ingredients/ingredientcreate.html'
+    success_url = reverse_lazy('ingredients:stocklist')
+
+    def get_context_data(self, **kwargs):
+        #Overwrite the method
+        context = super().get_context_data(**kwargs)
+        #Get aditional context
+        context['ingredients'] = Ingredient.objects.all()
+        context['stock'] = Stock.objects.all()
+ 
+        return context
 
 class StockView(LoginRequiredMixin, ListView):
     """Return all published posts."""
@@ -16,10 +33,24 @@ class StockView(LoginRequiredMixin, ListView):
     model = Stock
     queryset = Stock.objects.all()
     ordering = ('-created',)
-    context_object_name = 'ingredients'
+    context_object_name = 'stock'
+    
+    def get_context_data(self, **kwargs): 
+        context = super().get_context_data(**kwargs)
+        context['total_stock']= Stock.objects.first()
+        
+        return context
 
-class IngredientCreateView(LoginRequiredMixin, CreateView):
-    model = Ingredient
-    fields = ['name','units']
-    template_name = 'ingredients/ingredientcreate.html'
+class StockCreateView(LoginRequiredMixin, CreateView):
+    form_class = StockForm
+    template_name = 'ingredients/stockcreate.html'
     success_url = reverse_lazy('ingredients:stocklist')
+
+    def get_context_data(self, **kwargs):
+        #Overwrite the method
+        context = super().get_context_data(**kwargs)
+        #Get aditional context
+        context['ingredients'] = Ingredient.objects.all()
+        context['stock'] = Stock.objects.all()
+ 
+        return context
